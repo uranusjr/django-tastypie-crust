d2971af14f57e925e1500da9ede42adb34d0dc62
Tzu-ping Chung <uranusjr@gmail.com>
2013-12-13 13:38:33 +0800

    Change some naming in AnonymousAuthentication

--- tastycrust/authentication.py
+++ tastycrust/authentication.py
@@ -3,14 +3,11 @@
 
 
 class AnonymousAuthentication(object):
-    anonymous_allowed_methods = ['GET']
+    allowed_methods = ['GET']
 
     def __init__(self, allowed=None):
         if allowed is not None:
-            self.anonymous_allowed_methods = allowed
+            self.allowed_methods = allowed
 
     def is_authenticated(self, request, **kwargs):
-        allowed_methods = [s.upper() for s in self.anonymous_allowed_methods]
-        if request.method in allowed_methods:
-            return True
-        return False
+        return (request.method in [s.upper() for s in self.allowed_methods])
