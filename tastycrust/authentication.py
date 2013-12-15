59e647a48538dc9e67a9d1f2855edb56a71c55d7
Tzu-ping Chung <uranusjr@gmail.com>
2013-12-15 23:33:24 +0800

    Fix Authentication class inheritance

--- tastycrust/authentication.py
+++ tastycrust/authentication.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
+from tastypie.authentication import Authentication
 
-class AnonymousAuthentication(object):
+
+class AnonymousAuthentication(Authentication):
+
     allowed_methods = ['GET']
 
     def __init__(self, allowed=None):
         if allowed is not None:
-            self.allowed_methods = allowed
+            self.allowed_methods = [s.upper() for s in allowed]
 
     def is_authenticated(self, request, **kwargs):
-        return (request.method in [s.upper() for s in self.allowed_methods])
+        return (request.method in self.allowed_methods)
